5e8341153d7e91c0ad9254d8727d0e7279bca1e5
elkor260891 <93071995+elkor260891@users.noreply.github.com>
2021-10-24 16:46:51 +0200

    dodatkowa opcja jednoargumentowa liczba + operator, oraz obsługa wyjątków  w operacji dzielenia  dwuargunentowa (dwie liczby + operator)

--- RPN/RPN/RPN.cs
+++ RPN/RPN/RPN.cs
@@ -1,37 +1,53 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace RPNCalulator {
 	public class RPN {
-		private Stack<int> _operators;
-		Dictionary<string, Func<int, int, int>> _operationFunction;
+		private Stack<int> _operators; 
+		Dictionary<string, Func<int, int, int>> _operationFunctionTwoNumber; //  _operationFunction jako słownik dla 2
+		Dictionary<string, Func<int, int>> _operationFunctionSingleNumber; //  _operationFunction jako słownik dla 1
+		static Func<int, int> factorial = n => n < 2 ? 1 : n * factorial(n - 1);
 
-		public int EvalRPN(string input) {
-			_operationFunction = new Dictionary<string, Func<int, int, int>>
+		public int EvalRPN(string input) { // Funkcja wykonująca dla obu opcji jedno i dwu arg
+			_operationFunctionSingleNumber = new Dictionary<string, Func<int, int>> // dla int i znak -obiekt typu słownik
 			{
-				["+"] = (fst, snd) => (fst + snd),
-				["-"] = (fst, snd) => (fst - snd),
-				["*"] = (fst, snd) => (fst * snd),
-				["/"] = (fst, snd) => (fst / snd)
+				["!"] = (fst) => (fst < 0 ? throw new InvalidOperationException() : factorial(fst)),
+				["||"] = (fst) => (Math.Abs(fst))
 			};
-			_operators = new Stack<int>();
 
-			var splitInput = input.Split(' ');
+			_operationFunctionTwoNumber = new Dictionary<string, Func<int, int, int>> // nowy słownik dla dwóch
+			{
+				["+"] = (fst, snd) => (fst + snd), // Jeżeli kluczem jest + to weź argument fst oraz snd i wykonaj fst + snd
+				["-"] = (fst, snd) => (fst - snd), 
+				["*"] = (fst, snd) => (fst * snd), 
+				["/"] = (fst, snd) => (snd == 0 ? throw new DivideByZeroException() : fst < snd ? throw new InvalidOperationException() : fst / snd)
+				//  dzielenie z wyjątkami  gdy dziel przez 0, gdy pierwsza jest mniejsza niz druga
+			};
+			_operators = new Stack<int>(); // nowy stos
+
+			var splitInput = input.Split(' '); // dzieli input na pojedyncze znaki
 			foreach (var op in splitInput)
 			{
-				if (IsNumber(op))
-					_operators.Push(Int32.Parse(op));
+				if (IsNumber(op)) // Jeśli znak jest liczbą
+					_operators.Push(Int32.Parse(op)); // wrzuć  na stos
 				else
-				if (IsOperator(op))
+				if (IsOperatorForSingleNumber(op)) // Jeśli znak jest operatorem dla jednego argumentu np. silnia
 				{
-					var num1 = _operators.Pop();
+					var num = _operators.Pop(); // Zdejmij ze stosu 
+					_operators.Push(_operationFunctionSingleNumber[op](num)); //  na stos wynik działania w _operationFunction wg odczyt. operatora
+				}
+				else
+				if (IsOperatorForTwoNumbers(op)) // i operator dla dwóch arg i op
+				{
 					var num2 = _operators.Pop();
-					_operators.Push(_operationFunction[op](num1, num2));
-					//_operators.Push(Operation(op)(num1, num2));
+					var num1 = _operators.Pop(); 
+
+					_operators.Push(_operationFunctionTwoNumber[op](num1, num2)); // Wrzuć na stos wynik ( dwóch intów i operator )
+					
 				}
 			}
 
-			var result = _operators.Pop();
+			var result = _operators.Pop(); // result = zdejmij ze stosu wynik
 			if (_operators.IsEmpty)
 			{
 				return result;
@@ -39,11 +55,14 @@ namespace RPNCalulator {
 			throw new InvalidOperationException();
 		}
 
-		private bool IsNumber(String input) => Int32.TryParse(input, out _);
+		private bool IsNumber(String input) => Int32.TryParse(input, out _); // Funkcja sprawdzająca czy znak jest liczbą
 
-		private bool IsOperator(String input) =>
+		private bool IsOperatorForTwoNumbers(String input) => 
 			input.Equals("+") || input.Equals("-") ||
 			input.Equals("*") || input.Equals("/");
+
+		private bool IsOperatorForSingleNumber(String input) => 
+			input.Equals("!") || input.Equals("||");
 
 		private Func<int, int, int> Operation(String input) =>
 			(x, y) =>
